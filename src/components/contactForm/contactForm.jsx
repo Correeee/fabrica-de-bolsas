822ce936adf50ee26751b3ad6f9ccaf5458f91c8
elcorre <correa755@hotmail.com>
2023-11-05 00:25:22 -0300

    mail msg

--- src/components/contactForm/contactForm.jsx
+++ src/components/contactForm/contactForm.jsx
@@ -11,7 +11,7 @@ const ContactForm = () => {
     const handlerMail = () => {
 
         if (name, phone, text) {
-            const msg = `¡Hola BUSTA! Mi nombre es ${name.toUpperCase()} y me número de contacto ${phone}. ${text}`
+            const msg = `¡Hola BUSTA! Mi nombre es ${name.toUpperCase()} y mi número de contacto ${phone}. ${text}`
             setMessage(msg)
         }
 
